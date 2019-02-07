90960a1a8481b668283a7a3468a19cda6fff4ee2
Muhammed Enes Ates <atesmuhammedenes@gmail.com>
2019-02-07 21:30:29 +0300

    first commit

---
+++ src/test/java/com/menesates/serenitypratik/search/WhenSearchWord.java
@@ -0,0 +1,29 @@
+package com.menesates.serenitypratik.search;
+
+import com.menesates.serenitypratik.steps.GoogleSteps;
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Narrative;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(SerenityRunner.class)
+@Narrative(text = "{Arama islemi}")
+public class WhenSearchWord {
+
+    @Steps
+    GoogleSteps googleSteps;
+
+    @Test
+    public void searchingWord(){
+        // given
+        googleSteps.openMozillaAndGoogle();
+
+        // when
+        googleSteps.searchGoogle("serenity");
+
+
+        // then
+        googleSteps.checkTitle();
+    }
+}
---
+++ src/test/java/com/menesates/serenitypratik/steps/GoogleSteps.java
@@ -0,0 +1,24 @@
+package com.menesates.serenitypratik.steps;
+
+import net.thucydides.core.annotations.Step;
+
+public class GoogleSteps {
+
+    String actor;
+
+    @Step("#actor step 1")
+    public void openMozillaAndGoogle(){
+
+    }
+
+    @Step("#actor step2 {0}")
+    public void searchGoogle(String word){
+
+    }
+
+    @Step("#actor step3")
+    public void checkTitle(){
+
+    }
+
+}
